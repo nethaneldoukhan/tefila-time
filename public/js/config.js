3c577183056458308d51b9781329e82ad343b20e
Nati <natdoukhan@gmail.com>
2021-02-07 20:18:07 +0200

    rename the site

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,3 @@
-export const DOMAINE = 'https://mighty-chamber-84336.herokuapp.com';
+export const DOMAINE = 'https://tefila-time.herokuapp.com';
+// export const DOMAINE = 'https://mighty-chamber-84336.herokuapp.com';
 // export const DOMAINE = 'http://localhost:3000';
